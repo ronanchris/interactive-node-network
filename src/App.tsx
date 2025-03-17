6a025357a2a95596a9e2669964443c907fdff13f
ronanchris <chrisronan@me.com>
2025-03-17 13:54:48 -0500

    feat: add navigation bar with links to network and status dashboards

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Dashboard from './components/Dashboard'
+import Navigation from './components/Navigation'
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
+      <Navigation />
       <Dashboard />
     </div>
   )
